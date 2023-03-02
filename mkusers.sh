fadaf6625d72a7d59c4d72b4593431c130deaf98
Tersus <102046165+Terssuss@users.noreply.github.com>
2023-03-02 23:31:24 +0100

    Update mkusers.sh

--- mkusers.sh
+++ mkusers.sh
@@ -2,24 +2,28 @@
 
 # Creates all users with their full name and password included
 useradd -c "Jan Novak" -p uvodniheslo jnovak
-useradd -c "Jan Hrabal" -p uvodniheslo jhrabal 
+useradd -c "Jan Hrabal" -p uvodniheslo jhrabal
 useradd -c "Tereza Simakova" -p uvodniheslo tsimakova
 useradd -c "Martin Mazanek" -p uvodniheslo mmazanek
 useradd -c "Jitka Kuliskova" -p uvodniheslo jkuliskova
 
 # Creates all groups and adds users to them
 groupadd -U jnovak vyucujici
-groupadd -U jhrabal tsimakova mmazanek jkuliskova zaci
-groupadd -U jhrabal tsimakova mmazanek jkuliskova 1rocnik 
-groupadd -U tsimakova mmazanek jkuliskova 2rocnik 
-groupadd -U mmazanek, jkuliskova 3rocnik
+groupadd -U jhrabal,tsimakova,mmazanek,jkuliskova zaci
+groupadd -U jhrabal,tsimakova,mmazanek,jkuliskova 1rocnik
+groupadd -U tsimakova,mmazanek,jkuliskova 2rocnik
+groupadd -U mmazanek,jkuliskova 3rocnik
 groupadd -U jkuliskova 4rocnik
 
 # Sets the date on which the listed accounts will be disabled
 usermod -e 2022-09-01 jhrabal
-usermod -e 2021-09-01 tsimakova 
+usermod -e 2021-09-01 tsimakova
 usermod -e 2020-09-01 mmazanek
 usermod -e 2019-09-01 jkuliskova
 
-# Sets the password configuration
-passwd -e -n 5 -w 7 -x 180 {jnovak,jhrabal,tsimakova,mmazanek,jkuliskova}
+# Password configuration
+chage -d 0 -m 5 -M 180 -W 7 jnovak
+chage -d 0 -m 5 -M 180 -W 7 jhrabal
+chage -d 0 -m 5 -M 180 -W 7 tsimakova
+chage -d 0 -m 5 -M 180 -W 7 mmazanek
+chage -d 0 -m 5 -M 180 -W 7 jkuliskova
