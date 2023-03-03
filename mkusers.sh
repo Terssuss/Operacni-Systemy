af9cb155fd64f836e2819934cde67de37c8824cd
Tersus <102046165+Terssuss@users.noreply.github.com>
2023-03-03 08:37:52 +0100

    Update mkusers.sh

--- mkusers.sh
+++ mkusers.sh
@@ -8,11 +8,18 @@ useradd -c "Martin Mazanek" -p uvodniheslo mmazanek
 useradd -c "Jitka Kuliskova" -p uvodniheslo jkuliskova
 
 # Sets the password uvodniheslo for all the users
-usermod -p uvodniheslo jnovak
-usermod -p uvodniheslo jhrabal
-usermod -p uvodniheslo tsimakova
-usermod -p uvodniheslo mmazanek
-usermod -p uvodniheslo jkuliskova
+echo "uvodniheslo" | usermod -p - jnovak
+echo "uvodniheslo" | usermod -p - jhrabal
+echo "uvodniheslo" | usermod -p - tsimakova
+echo "uvodniheslo" | usermod -p - mmazanek
+echo "uvodniheslo" | usermod -p - jkuliskova
+
+# Password configuration
+chage -d 0 -m 5 -M 180 -W 7 jnovak
+chage -d 0 -m 5 -M 180 -W 7 jhrabal
+chage -d 0 -m 5 -M 180 -W 7 tsimakova
+chage -d 0 -m 5 -M 180 -W 7 mmazanek
+chage -d 0 -m 5 -M 180 -W 7 jkuliskova
 
 # Creates all groups and adds users to them
 groupadd -U jnovak vyucujici
@@ -27,10 +34,3 @@ usermod -e 2022-09-01 jhrabal
 usermod -e 2021-09-01 tsimakova
 usermod -e 2020-09-01 mmazanek
 usermod -e 2019-09-01 jkuliskova
-
-# Password configuration
-chage -d 0 -m 5 -M 180 -W 7 jnovak
-chage -d 0 -m 5 -M 180 -W 7 jhrabal
-chage -d 0 -m 5 -M 180 -W 7 tsimakova
-chage -d 0 -m 5 -M 180 -W 7 mmazanek
-chage -d 0 -m 5 -M 180 -W 7 jkuliskova
